91f6fbc2e8c79dab32cdb83848f9d99b2b1d1165
Danilo.Dev <182436555+DaniloSRocha26@users.noreply.github.com>
2026-01-09 11:37:10 -0300

    Adição dos MiddleWares express

--- NodeJs/modules/express.js
+++ NodeJs/modules/express.js
@@ -5,6 +5,15 @@ const UserModel = require("../src/models/user.model");
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(`Request-Type: ${req.method}`);
+  console.log(`Content-Type: ${req.headers["content-type"]}`);
+  console.log(`Date: ${new Date()}`);
+
+  next();
+});
+
+//Buscar usuários
 app.get("/users", async (req, res) => {
   try {
     const users = await UserModel.find({});
@@ -15,6 +24,7 @@ app.get("/users", async (req, res) => {
   }
 });
 
+//Busca usuários por ID
 app.get("/users/:id", async (req, res) => {
   try {
     const id = req.params.id;
@@ -27,11 +37,36 @@ app.get("/users/:id", async (req, res) => {
   }
 });
 
+//Criação dos Usuários
 app.post("/users", async (req, res) => {
   try {
     const user = await UserModel.create(req.body);
 
     res.status(201).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+//Atualizar dados do usuário
+app.patch("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const user = await UserModel.findByIdAndDelete(id);
+
+    res.status(200).json(user);
   } catch (error) {
     res.status(500).send(error.message);
   }
